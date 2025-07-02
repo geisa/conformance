d76ab66b51d702bd5a6d2e2fc41e7d37b3162bee
Kévin L'hôpital <kevin.lhopital@savoirfairelinux.com>
2025-07-02 15:10:18 +0200

    launch_conformance_tests_serial: use pexpect to send serial commands
    To simplify the code and have more control over the serial
    communication, this commit replaces the direct serial communication with
    `pexpect` commands.
    
    SFL: #17868
    Change-Id: I0a91530ab8763be737ef63a33840535463639d9c
    Signed-off-by: Kévin L'hôpital <kevin.lhopital@savoirfairelinux.com>

--- src/launch_conformance_tests_serial.py
+++ src/launch_conformance_tests_serial.py
@@ -12,7 +12,6 @@ import os
 import subprocess
 import sys
 import tarfile
-import time
 import pexpect
 import pexpect.fdpexpect
 import serial
@@ -26,17 +25,6 @@ def compress_dir(src_dir, archive_name):
     """
     with tarfile.open(f"{TOPDIR}/{archive_name}", "w:gz") as tar:
         tar.add(src_dir, arcname=os.path.basename(src_dir))
-
-
-def send_command(command, ser):
-    """
-    Send a command to the serial port.
-    """
-    ser.write((command + "\n").encode("utf-8"))
-    ser.flush()
-    ser.reset_input_buffer()
-    ser.reset_output_buffer()
-    time.sleep(1)
 
 
 def send_file_via_zmodem(archive_name, args):
@@ -56,40 +44,52 @@ def send_file_via_zmodem(archive_name, args):
         sys.exit(-1)
 
 
-def send_and_uncompress_cukinia_files(ser, args):
+def send_and_uncompress_cukinia_files(p, args):
     """
     Send and uncompress Cukinia files on the board.
     """
-    send_command("cd /tmp && rz -y --zmodem", ser)
-    send_file_via_zmodem(f"{TOPDIR}/cukinia.tar.gz", args)
-    send_command("cd /tmp && rz -y --zmodem", ser)
-    send_file_via_zmodem(f"{TOPDIR}/cukinia-tests.tar.gz", args)
+    try:
+        p.sendline("cd /tmp && rz -y --zmodem")
+        send_file_via_zmodem(f"{TOPDIR}/cukinia.tar.gz", args)
+        p.expect(r"(\S*)@\S*:.+(#|\$)")
+        p.sendline("cd /tmp && rz -y --zmodem")
+        send_file_via_zmodem(f"{TOPDIR}/cukinia-tests.tar.gz", args)
+        p.expect(r"(\S*)@\S*:.+(#|\$)")
 
-    send_command("mkdir -p conformance_tests", ser)
-    send_command("cd /tmp && tar -xzf cukinia.tar.gz -C /tmp/conformance_tests", ser)
-    send_command(
-        "cd /tmp && tar -xzf cukinia-tests.tar.gz -C /tmp/conformance_tests", ser
-    )
+        p.sendline("mkdir -p conformance_tests")
+        p.expect(r"(\S*)@\S*:.+(#|\$)")
+        p.sendline("cd /tmp && tar -xzf cukinia.tar.gz -C /tmp/conformance_tests")
+        p.expect(r"(\S*)@\S*:.+(#|\$)")
+        p.sendline("cd /tmp && tar -xzf cukinia-tests.tar.gz -C /tmp/conformance_tests")
+        p.expect(r"(\S*)@\S*:.+(#|\$)")
+    except pexpect.exceptions.TIMEOUT:
+        print("Error: Failed to copy test files to board.")
+        sys.exit(-1)
 
 
-def cleanup(ser):
+def cleanup(p):
     """
     Clean up the test files on the board.
     """
-    send_command(
-        "cd /tmp && rm -rf /tmp/conformance_tests /tmp/cukinia.tar.gz /tmp/cukinia-tests.tar.gz",
-        ser,
-    )
+    try:
+        p.sendline(
+            "cd /tmp && rm -rf /tmp/conformance_tests /tmp/cukinia.tar.gz "
+            "/tmp/cukinia-tests.tar.gz && echo 'Cleanup done'"
+        )
+        p.expect(r"Cleanup done")
+        p.expect(r"(\S*)@\S*:.+(#|\$)")
+    except pexpect.exceptions.TIMEOUT:
+        print("Error: Failed to clean up test files on board.")
+        sys.exit(-1)
 
 
-def get_cukinia_report(args, ser):
+def get_cukinia_report(args, p):
     """
     Get the Cukinia test report from the board.
     """
     print("Copying tests report on host\n")
-    send_command(
+    p.sendline(
         "cd /tmp/conformance_tests/cukinia-tests/ && sz -v geisa-conformance-report.xml --zmodem",
-        ser,
     )
     try:
         with open(args.serial, "rb") as ser_in, open(args.serial, "wb") as ser_out:
@@ -100,60 +100,69 @@ def get_cukinia_report(args, ser):
                 cwd=f"{TOPDIR}/reports",
                 check=True,
             )
+        p.expect(r"Transfer complete")
     except subprocess.CalledProcessError as e:
         print(f"Error receiving report file via ZMODEM: {e}")
         sys.exit(-1)
-
-    while True:
-        sz_output = ser.readline()
-        if "Transfer complete" in sz_output.decode("utf-8"):
-            break
+    except pexpect.exceptions.TIMEOUT:
+        print("Error: Failed to copy test report from board.")
+        sys.exit(-1)
 
 
-def print_cukinia_output(ser):
+def print_cukinia_output(p):
     """
     Print the output of the Cukinia tests from the board.
     """
-    ser.readline()
+    cukinia_output = p.readline()
     while True:
-        cukinia_output = ser.readline()
+        cukinia_output = p.readline()
+        if "--- GEISA conformance tests ---" in cukinia_output.decode("utf-8"):
+            break
+
+    print(cukinia_output.decode("utf-8").strip())
+    while True:
+        cukinia_output = p.readline()
         print(cukinia_output.decode("utf-8").strip())
         if "GEISA conformance tests" in cukinia_output.decode("utf-8"):
             break
 
 
-def get_cukinia_return_code(ser):
+def get_cukinia_return_code(p):
     """
     Get the return code of the Cukinia tests from the board.
     """
-    send_command("echo $?", ser)
-    ser.readline()
-    test_exit_code = ser.readline().decode("utf-8").strip()
+    p.sendline("echo $?")
+    p.expect([r"\d+"])
+    test_exit_code = p.match.group(0).decode()
     return int(test_exit_code)
 
 
-def launch_cukinia_tests(args, ser):
+def launch_cukinia_tests(p, args):
     """
     Launch Cukinia tests on the board.
     If --no-reports is specified, only run tests and display results without generating reports.
     """
-    if args.no_reports:
-        send_command(
-            "/tmp/conformance_tests/cukinia/cukinia "
-            "/tmp/conformance_tests/cukinia-tests/cukinia.conf",
-            ser,
-        )
-        print_cukinia_output(ser)
-        test_exit_code = get_cukinia_return_code(ser)
-    else:
-        send_command(
-            "/tmp/conformance_tests/cukinia/cukinia -f junitxml "
-            "-o /tmp/conformance_tests/cukinia-tests/geisa-conformance-report.xml "
-            "/tmp/conformance_tests/cukinia-tests/cukinia.conf",
-            ser,
-        )
-        test_exit_code = get_cukinia_return_code(ser)
-        get_cukinia_report(args, ser)
+    try:
+        if args.no_reports:
+            p.sendline(
+                "/tmp/conformance_tests/cukinia/cukinia "
+                "/tmp/conformance_tests/cukinia-tests/cukinia.conf"
+            )
+            print_cukinia_output(p)
+            p.expect(r"(\S*)@\S*:.+(#|\$)")
+            test_exit_code = get_cukinia_return_code(p)
+        else:
+            p.sendline(
+                "/tmp/conformance_tests/cukinia/cukinia -f junitxml "
+                "-o /tmp/conformance_tests/cukinia-tests/geisa-conformance-report.xml "
+                "/tmp/conformance_tests/cukinia-tests/cukinia.conf"
+            )
+            p.expect(r"(\S*)@\S*:.+(#|\$)")
+            test_exit_code = get_cukinia_return_code(p)
+            get_cukinia_report(args, p)
+    except pexpect.exceptions.TIMEOUT:
+        print("Error: Failed to launch Cukinia tests.")
+        sys.exit(-1)
 
     return int(test_exit_code)
 
@@ -240,16 +249,16 @@ def main():
             detect_board_state(p, args)
 
             print("Cleaning previous test results on board\n")
-            cleanup(ser)
+            cleanup(p)
 
             print("Copying conformance test files to board\n")
-            send_and_uncompress_cukinia_files(ser, args)
+            send_and_uncompress_cukinia_files(p, args)
 
             print("Launching tests...\n")
-            test_exit_code = launch_cukinia_tests(args, ser)
+            test_exit_code = launch_cukinia_tests(p, args)
 
             print("Cleaning up test files on board\n")
-            cleanup(ser)
+            cleanup(p)
 
     except serial.SerialException as e:
         print(f"Error when executing serial command: {e}\n")
