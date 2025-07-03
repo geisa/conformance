fa41687e868f3fb203d07abce33496d4c927aa04
Kévin L'hôpital <kevin.lhopital@savoirfairelinux.com>
2025-07-03 16:41:08 +0200

    cukinia-tests: basic_hardware_tests: use script to get root device major
    This commit refactors the way we get the root device major number
    to use a dedicated script. This change improves code readability and
    add the possibility to reuse this logic in other tests without
    duplicating code.
    
    SFL: #17872
    Change-Id: I45d640705c7e460de5f67e54cf7f6fa48a25dc58
    Signed-off-by: Kévin L'hôpital <kevin.lhopital@savoirfairelinux.com>

---
+++ src/cukinia-tests/tests.d/get_root_device_major.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#
+# Copyright (C) 2025 Southern California Edison
+
+ROOT_DEVICE_MAJOR=$(mountpoint -d / | awk '{ split($1, x, ":"); print x[1] }')
+if [ "${ROOT_DEVICE_MAJOR}" -eq 0 ]; then
+	ROOT_DEVICE=$(cat /proc/mounts | awk '$2 == "/" { print $1 }')
+
+	if [ $(echo "${ROOT_DEVICE}" | grep -c "mapper") -eq 1 ]; then
+		ROOT_DEVICE=$(readlink -f "${ROOT_DEVICE}")
+	fi
+	if [ -d "/sys/class/block/$(basename ${ROOT_DEVICE})" ]; then
+		ROOT_DEVICE="/dev/$(ls /sys/class/block/$(basename ${ROOT_DEVICE})/slaves)"
+	fi
+	ROOT_DEVICE_MAJOR=$(mountpoint -x "${ROOT_DEVICE}" | awk '{ split($1, x, ":"); print x[1] }')
+fi
