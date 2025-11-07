6a7d8acd473348f4089b09f85362fad040e57a98
Kévin L'hôpital <kevin.lhopital@savoirfairelinux.com>
2025-11-07 17:36:13 +0100

    GEISA-API-tests: add gapi_connection test program
    Add a new test program gapi_connection to test the connection and
    disconnection to an MQTT broker using the GEISA API.
    
    SFL: #19180
    Change-Id: I3f8f60063bf0c392c73f65fabd1b0b8f41448d0a
    Signed-off-by: Kévin L'hôpital <kevin.lhopital@savoirfairelinux.com>

---
+++ src/GEISA-API-tests/src/gapi_connection.c
@@ -0,0 +1,34 @@
+/**
+ * @file gapi_connection.c
+ * @brief Test connection and deconnection of the MQTT broker
+ * @copyright Copyright (C) 2025 Southern California Edison
+ */
+#include "gapi_mosquitto.h"
+
+volatile bool running = true;
+volatile bool isConnected = false;
+
+int main(int argc, char **argv)
+{
+	if(argc < 2) {
+		fprintf(stderr, "Usage:\n  %s <broker>\n", argv[0]);
+		return 1;
+	}
+
+	const char *broker = argv[1];
+	struct mosquitto *mosq;
+	int port = 1883;
+
+	mosq = api_communication_init(broker, port);
+	if (!mosq) {
+		return EXIT_FAILURE;
+	}
+
+	while (running && ! isConnected) {
+		mosquitto_loop(mosq, -1, 1);
+		sleep(1);
+	}
+
+	api_communication_deinit(mosq);
+	return EXIT_SUCCESS;
+}
