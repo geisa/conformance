50ea4a324610e6dec2bcb4cbef7e6fa1782d848b
Kévin L'hôpital <kevin.lhopital@savoirfairelinux.com>
2025-11-14 14:46:12 +0100

    launch_gapi_conformance_tests: define SSH and SCP functions
    Define SSH and SCP functions to simplify remote command execution and
    file transfer.
    
    SFL: #19181
    Change-Id: I2c9a79ef167267bb3719747a7988c17e7eb65042
    Signed-off-by: Kévin L'hôpital <kevin.lhopital@savoirfairelinux.com>

--- src/launch_gapi_conformance_tests.sh
+++ src/launch_gapi_conformance_tests.sh
@@ -6,6 +6,21 @@
 # GEISA Conformance is free software, distributed under the Apache License
 # version 2.0. See LICENSE for details.
 
+RED="\e[31m"
+ENDCOLOR="\e[0m"
+declare CONFORMANCE_SSH_ARGS
+declare CONFORMANCE_SCP_ARGS
+
+SSH() {
+	#shellcheck disable=SC2086
+	sshpass -p "${board_password}" ssh ${CONFORMANCE_SSH_ARGS} -tt -o LogLevel=QUIET -o StrictHostKeyChecking=no "${board_user}@${board_ip}" "$@"
+}
+
+SCP() {
+	#shellcheck disable=SC2086
+	sshpass -p "${board_password}" scp ${CONFORMANCE_SCP_ARGS} -o StrictHostKeyChecking=no "$@"
+}
+
 launch_gapi_tests_with_report() {
 	local topdir="$1"
 
