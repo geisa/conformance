7c9bf8d1d89e2e8fd2e293ca3e74fb221b990a34
Kévin L'hôpital <kevin.lhopital@savoirfairelinux.com>
2026-06-04 10:12:37 +0200

    GEISA-API-tests:gapi_instantaneous: update tests to check for discovery
    All the device don't have all the phase information, it depends on the
    device type. Then the test should check the discovery information to
    know which phase information should be expected in the instantaneous
    response. Add a new callback to check the discovery information and save
    the relevant information in a context struct that can be used by the
    instantaneous message callback.
    
    SFL: #21956
    Change-Id: I6ce318706906f2177ebd476a7bb5cc3a218a99ab
    Signed-off-by: Kévin L'hôpital <kevin.lhopital@savoirfairelinux.com>

--- src/GEISA-API-tests/src/gapi_instantaneous.c
+++ src/GEISA-API-tests/src/gapi_instantaneous.c
@@ -3,28 +3,166 @@
  * @brief Test instantaneous API messages
  * @copyright Copyright (C) 2026 Southern California Edison
  */
+
+#include "gapi_discovery.h"
 #include "gapi_mosquitto.h"
 #include "pb.h"
 #include "pb_decode.h"
 #include "pb_encode.h"
 #include "schemas/metered_quantities.pb.h"
+#include <stdbool.h>
 
 volatile bool running = true;
 volatile bool isConnected = false;
 volatile bool rr_disconnect = false;
 const int TIMEOUT_S = 5;
 
+/**
+ * @brief Context structure for the instantaneous test, used to store
+ * information about the device under test and the test result.
+ */
+struct instantaneous_test_ctx {
+	int test_result;
+	bool is_meter;
+	uint32_t phase_count;
+	bool neutral_connected;
+};
+
+/**
+ * @brief Callback function to get the discovery information of the device under
+ * test. This function decodes the discovery response and updates the context
+ * with information about the device.
+ *
+ * @param mosq The mosquitto client instance
+ * @param obj The user data object, which is a pointer to the instantaneous test
+ * context
+ * @param msg The MQTT message containing the discovery response
+ */
+static void get_discovery_information(struct mosquitto *mosq, void *obj,
+				      const struct mosquitto_message *msg)
+{
+	GeisaPlatformDiscovery_Rsp response =
+	    GeisaPlatformDiscovery_Rsp_init_default;
+	struct instantaneous_test_ctx *ctx = obj;
+	pb_istream_t istream;
+	bool status = false;
+	(void)mosq;
+
+	ctx->is_meter = false;
+	ctx->phase_count = 0;
+	ctx->neutral_connected = false;
+
+	istream = pb_istream_from_buffer(msg->payload, msg->payloadlen);
+	status =
+	    pb_decode(&istream, GeisaPlatformDiscovery_Rsp_fields, &response);
+
+	if (!status) {
+		fprintf(stderr,
+			"[Instantaneous] Error decoding discovery response\n");
+		ctx->test_result = EXIT_FAILURE;
+		goto disconnect;
+	}
+
+	if (response.has_metrology == false) {
+		goto disconnect;
+	}
+
+	if (response.device.top_module.type ==
+	    GeisaPlatformDiscovery_DeviceType_TYPE_ELECTRIC_METER) {
+		ctx->is_meter = true;
+		ctx->phase_count = response.metrology.phase_count;
+		ctx->neutral_connected = response.metrology.neutral_connected;
+	}
+
+disconnect:
+	pb_release(GeisaPlatformDiscovery_Rsp_fields, &response);
+	rr_disconnect = true;
+}
+
+/**
+ * @brief Function to check the instantaneous response for a meter. This
+ * function checks that the response contains the expected messages when the
+ * device is a meter.
+ *
+ * @param ctx The context of the instantaneous test, containing information
+ * about the device under test
+ * @param response The decoded instantaneous response from the meter
+ */
+static void
+check_meter_instantaneous_response(struct instantaneous_test_ctx *ctx,
+				   GeisaInstantaneousQuantities *response)
+{
+	if (ctx->phase_count == 1) {
+		if (response->has_phase_A == false) {
+			fprintf(
+			    stderr,
+			    "[Instantaneous] Error: single phase meter instantaneous response missing phase_A message\n");
+			ctx->test_result = EXIT_FAILURE;
+		}
+	} else if (ctx->phase_count == 2) {
+		if (response->has_phase_A == false) {
+			fprintf(
+			    stderr,
+			    "[Instantaneous] Error: two phase meter instantaneous response missing phase_A message\n");
+			ctx->test_result = EXIT_FAILURE;
+		}
+		if (response->has_phase_B == false) {
+			fprintf(
+			    stderr,
+			    "[Instantaneous] Error: two phase meter instantaneous response missing phase_B message\n");
+			ctx->test_result = EXIT_FAILURE;
+		}
+	} else if (ctx->phase_count == 3) {
+		if (response->has_phase_A == false) {
+			fprintf(
+			    stderr,
+			    "[Instantaneous] Error: three phase meter instantaneous response missing phase_A message\n");
+			ctx->test_result = EXIT_FAILURE;
+		}
+		if (response->has_phase_B == false) {
+			fprintf(
+			    stderr,
+			    "[Instantaneous] Error: three phase meter instantaneous response missing phase_B message\n");
+			ctx->test_result = EXIT_FAILURE;
+		}
+		if (response->has_phase_C == false) {
+			fprintf(
+			    stderr,
+			    "[Instantaneous] Error: three phase meter instantaneous response missing phase_C message\n");
+			ctx->test_result = EXIT_FAILURE;
+		}
+	}
+	if (ctx->neutral_connected) {
+		if (response->has_phase_N == false) {
+			fprintf(
+			    stderr,
+			    "[Instantaneous] Error: meter with neutral connected instantaneous response missing neutral message\n");
+			ctx->test_result = EXIT_FAILURE;
+		}
+	}
+}
+
+/**
+ * @brief Callback function to check the instantaneous response message. This
+ * function decodes the instantaneous response and checks that it contains the
+ * expected information based on the device type and discovery information.
+ *
+ * @param mosq The mosquitto client instance
+ * @param obj The user data object, which is a pointer to the instantaneous test
+ * context
+ * @param msg The MQTT message containing the instantaneous response
+ */
 static void check_instantaneous_message(struct mosquitto *mosq, void *obj,
 					const struct mosquitto_message *msg)
 {
 	GeisaInstantaneousQuantities response =
 	    GeisaInstantaneousQuantities_init_default;
 	(void)mosq;
-	int *test_result = obj;
+	struct instantaneous_test_ctx *ctx = obj;
 	pb_istream_t istream;
 	bool status = false;
 
-	*test_result = EXIT_SUCCESS;
+	ctx->test_result = EXIT_SUCCESS;
 
 	istream = pb_istream_from_buffer(msg->payload, msg->payloadlen);
 	status =
@@ -34,51 +172,50 @@ static void check_instantaneous_message(struct mosquitto *mosq, void *obj,
 		fprintf(
 		    stderr,
 		    "[Instantaneous] Error decoding instantaneous response\n");
-		*test_result = EXIT_FAILURE;
+		ctx->test_result = EXIT_FAILURE;
 		goto disconnect;
 	}
 
-	if (response.has_phase_A == false) {
+	if (response.timestamp == 0) {
 		fprintf(stderr, "[Instantaneous] Error: instantaneous response "
-				"missing phase_A message\n");
-		*test_result = EXIT_FAILURE;
+				"missing timestamp information\n");
+		ctx->test_result = EXIT_FAILURE;
 	}
 
-	if (response.has_phase_B == false) {
-		fprintf(stderr, "[Instantaneous] Error: instantaneous response "
-				"missing phase_B message\n");
-		*test_result = EXIT_FAILURE;
+	if (ctx->is_meter) {
+		check_meter_instantaneous_response(ctx, &response);
 	}
 
-	if (response.has_phase_C == false) {
-		fprintf(stderr, "[Instantaneous] Error: instantaneous response "
-				"missing phase_C message\n");
-		*test_result = EXIT_FAILURE;
-	}
-
-	if (response.has_phase_N == false) {
-		fprintf(stderr, "[Instantaneous] Error: instantaneous response "
-				"missing phase_N message\n");
-		*test_result = EXIT_FAILURE;
-	}
 	if (response.has_other == false) {
-		fprintf(stderr, "[Instantaneous] Error: instantaneous response "
-				"missing other message\n");
-		*test_result = EXIT_FAILURE;
+		fprintf(
+		    stderr,
+		    "[Instantaneous] Error: instantaneous response missing other message\n");
+		ctx->test_result = EXIT_FAILURE;
 	}
 
 disconnect:
 	pb_release(GeisaInstantaneousQuantities_fields, &response);
-	printf("[Instantaneous] test_result: %d\n", *test_result);
+	printf("[Instantaneous] test_result: %d\n", ctx->test_result);
 	running = false;
 }
 
+/**
+ * @brief Main function for the instantaneous API test. This function
+ * initializes the MQTT communication, sends a discovery request, and checks the
+ * instantaneous response messages.
+ *
+ * @return EXIT_SUCCESS if the test passes, EXIT_FAILURE if the test fails
+ */
 int main()
 {
 	struct mosquitto *mosq = NULL;
 	int return_code = 0;
-	int test_result = 0;
 	time_t start = 0;
+
+	struct instantaneous_test_ctx *ctx =
+	    calloc(1, sizeof(struct instantaneous_test_ctx));
+
+	ctx->test_result = 0;
 
 	mosq = api_communication_init();
 	if (!mosq) {
@@ -86,9 +223,7 @@ int main()
 		goto exit;
 	}
 
-	mosquitto_message_callback_set(mosq, check_instantaneous_message);
-
-	mosquitto_user_data_set(mosq, &test_result);
+	mosquitto_user_data_set(mosq, ctx);
 
 	start = time(NULL);
 	while (running && !isConnected) {
@@ -108,9 +243,22 @@ int main()
 		goto disconnect;
 	}
 
+	mosquitto_message_callback_set(mosq, get_discovery_information);
+	return_code = send_discovery_request(mosq);
+	if (return_code == EXIT_FAILURE) {
+		fprintf(stderr,
+			"[Instantaneous] Error sending discovery request\n");
+		goto disconnect;
+	}
+
+	// Set running back to true after sending the discovery request
+	running = true;
+
+	mosquitto_message_callback_set(mosq, check_instantaneous_message);
+
 	return_code = api_subscribe(mosq, "geisa/api/instantaneous/data", 0);
 	if (return_code != MOSQ_ERR_SUCCESS) {
-		fprintf(stderr, "[Discovery] Error subscribing to "
+		fprintf(stderr, "[Instantaneous] Error subscribing to "
 				"geisa/api/instantaneous/data topic\n");
 		return_code = EXIT_FAILURE;
 		goto disconnect;
@@ -130,5 +278,7 @@ int main()
 disconnect:
 	api_communication_deinit(mosq);
 exit:
-	return return_code || test_result;
+	return_code = return_code ? return_code : ctx->test_result;
+	free(ctx);
+	return return_code;
 }
